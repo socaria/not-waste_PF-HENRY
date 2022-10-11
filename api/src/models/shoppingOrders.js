483c1f08926d8f8a0ef4551b6ac9d6f0c7a58012
Dedwison <dedwison@gmail.com>
2022-10-11 20:05:06 -0300

    modelos: manager, shoppingOrders, storeCategories

--- api/src/models/shoppingOrders.js
+++ api/src/models/shoppingOrders.js
@@ -1,14 +1,14 @@
 const { DataTypes } = requiere("sequelize");
 
 module.export = (sequelize) => {
-  sequelize.difine("manager", {
-    user: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    password: {
+  sequelize.difine("shoppingOrders", {
+    customerId: {
       type: DataTypes.NUMBER,
       allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      defaultValue: DataTypes.NOW,
     },
   });
 };
