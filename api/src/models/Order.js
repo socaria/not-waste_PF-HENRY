c9868f8e0e272b089054912b4030414f0f9cce96
Sofia Caria <103151285+socaria@users.noreply.github.com>
2022-10-12 22:34:33 +0200

    Modifico Order agregando propiedades state y review

--- api/src/models/Order.js
+++ api/src/models/Order.js
@@ -11,5 +11,15 @@ module.exports = (sequelize) => {
       type: DataTypes.DATEONLY,
       defaultValue: DataTypes.NOW,
     },
+    state: {
+      type: DataTypes.ENUM('pendiente', 'confirmado', 'entregado', 'cancelado'),
+      defaultValue: 'pendiente'
+    },
+    review: {
+      type: DataTypes.INTEGER,
+      min: 0,
+      max: 5,
+      // TODO agregar condición de que sólo se puede modificar cuando el pedido está en estado entregado
+    }
   });
 };
