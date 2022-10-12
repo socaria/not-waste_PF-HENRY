28d762e893fd7210d4955014c90f31bd3c1831c2
Sofia Caria <103151285+socaria@users.noreply.github.com>
2022-10-12 19:14:03 +0200

    modifico manager

--- api/src/models/manager.js
+++ api/src/models/manager.js
@@ -9,7 +9,7 @@ module.exports = (sequelize) => {
     },
     username: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
     password: {
       type: DataTypes.STRING,
