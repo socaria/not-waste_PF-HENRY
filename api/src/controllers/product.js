64c0b265292952732e6e0918afe51f306f7a84e6
Sofia Caria <103151285+socaria@users.noreply.github.com>
2022-10-14 10:51:59 +0200

    ruta post de product terminada

--- api/src/controllers/product.js
+++ api/src/controllers/product.js
@@ -1,38 +1,12 @@
-const { Router } = require("express");
-const { Product } = require("../db");
-
-const getProductByCity = async (req, res) => {
-    // const { city } = req.params;
-    // let products = await Product.findAll({ where: { city: city } });
-    // //   console.log(products);
-    // if (products.length) {
-    //     res.status(200).send(products);
-    // } else {
-    //     res.status(404).json("No se encontraron proveedores en esa ciudad");
-    // }
-};
+const { Product, Diet } = require("../db");
 
 // Ruta get va a buscar a todos los proveedores de la base de datos. Si llegasen las propiedades
 // name o store cateogry por query se retornarán los proveedores que coincidan con lo solicitado
 const getProducts = async (req, res) => {
-    const { name, category, city } = req.query;
+    const { diet, category } = req.query;
     let products;
     try {
         products = await Product.findAll();
-        if (name) {
-            products = products.filter(s => {
-                s.name === name
-            });
-            if (!products.length) {
-                throw new Error('No hay proveedores con ese nombre')
-            };
-        }
-        if (category) {
-            products = products.filter(s => s.category === category)
-            if (!products.length) {
-                throw new Error('No hay proveedores con esa categoría de establecimiento')
-            };
-        }
         res.status(200).send(products);
     } catch (e) {
         res.status(404).send(e.message);
@@ -42,35 +16,35 @@ const getProducts = async (req, res) => {
 const postProduct = async (req, res) => {
     let {
         name,
-        password,
-        phone,
-        email,
-        adress,
-        cuit,
+        price,
+        realValue,
+        description,
+        stock,
         imagen,
-        city,
-        category,
-        enabled
+        diet
     } = req.body
 
     try {
-        if (!name) { throw new Error('El campo del nombre del establecimiento es obligatorio') }
-        if (!password) { throw new Error('La contraseña debe ser definida') }
-        if (!phone) { throw new Error('El campo del teléfono es obligatorio') }
-        if (!email) { throw new Error('El campo del e-mail es obligatorio') }
-        if (!city) { throw new Error('El campo de la ciudad es obligatorio') }
+        if (!name) { throw new Error('Debe definirse un nombre') }
+        if (!price) { throw new Error('Debe definirse un precio') }
+        if (!realValue) { throw new Error('Debe definirse un valor real') }
+        if (!description) { throw new Error('Debe definirse una descripción') }
+        if (!stock) { throw new Error('Debe definirse un stock') }
         let newProduct = await Product.create({
             name,
-            password,
-            phone,
-            email,
-            adress,
-            cuit,
-            imagen,
-            enabled,
-            city,
-            category
+            price,
+            realValue,
+            description,
+            stock,
+            imagen
         })
+        let dietDb = await Diet.findAll({
+            where: {
+                name: diet,
+            },
+        });
+
+        newProduct.addDiets(dietDb);
 
         res.send(newProduct);
 
@@ -94,7 +68,7 @@ const putProduct = async (req, res) => {
         enabled
     } = req.body;
     let productToModify = await Product.findByPk(id)
-        try {
+    try {
         if (!productToModify) { throw new Error('No hay proveedores con ese ID') }
         if (!name) { throw new Error('El campo del nombre del establecimiento es obligatorio') }
         if (!password) { throw new Error('La contraseña debe ser definida') }
@@ -138,10 +112,8 @@ const deleteProduct = async (req, res) => {
 }
 
 module.exports = {
-    getProductByCity,
     getProducts,
     postProduct,
     putProduct,
     deleteProduct
-
 };
