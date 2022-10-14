082707c7260fbaa31aa3539dc9a03ea57c89b087
SerCM <serranocristian@hotmail.com.ar>
2022-10-14 03:44:16 -0300

    visualizacion de datos de perfil

--- client/src/router/RouteController.jsx
+++ client/src/router/RouteController.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Registrering from "../component/Registrering/Registrering";
 import LandingPage from '../component/LandingPage/LandingPage';
 import Home from "../component/Home/Home.jsx";
+import Profile from "../component/Profile";
 
 function RouteController (){
     return(
@@ -10,6 +11,7 @@ function RouteController (){
                 <Route exact path='/' element={<LandingPage/>} />
                 <Route exact path='/register' element={<Registrering />} />
                 <Route exact path='/home' element={<Home />} />
+                <Route exact path='/profile' element={<Profile />} />
             </Routes>
     )
 }
