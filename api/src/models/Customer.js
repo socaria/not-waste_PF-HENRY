15fcb56c025c96ec3b10c27f973eab63d4331ed7
Sofia Caria <103151285+socaria@users.noreply.github.com>
2022-10-14 20:16:04 +0200

    se modifica modelo Customer para implementar auth0

--- api/src/models/Customer.js
+++ api/src/models/Customer.js
@@ -11,17 +11,14 @@ module.exports = (sequelize) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-      primaryKey: true,
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      primaryKey: true,
     },
     image: {
       type: DataTypes.STRING,
       allowNull: false,
-      primaryKey: true,
     },
     // TODO cambiar datatype a ENUM con los partidos de buenos aires
    
