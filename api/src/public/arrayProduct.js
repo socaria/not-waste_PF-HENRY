a961daf62471e666ff808f2a9e3319f49bbab199
Dedwison <dedwison@gmail.com>
2022-10-13 21:27:42 -0300

    products json

--- api/src/public/arrayProduct.js
+++ api/src/public/arrayProduct.js
@@ -11,6 +11,30 @@ const arrayProduct = [
     imagen:
       "https://www.recetasderechupete.com/wp-content/uploads/2020/10/Ensalada-Cesar-final_-768x530.jpg",
   },
+  {
+    id: 02,
+    name: "papas fritas",
+    price: 120,
+    realValue: 500,
+    description:
+      "Las papas fritas o patatas fritas, también conocidas como papas a la belga, patatas a la belga o papas francesas, son las papas que se preparan cortándose en forma de bastones y friéndolas en aceite caliente hasta que queden doradas, crujientes para luego retirarlas del aceite y luego sazonarlas con sal.",
+    stock: 5,
+    min: 2,
+    imagen:
+      "https://www.mydibel.be/sites/default/files/styles/lightbox/public/images/Product_Shoestring_0.jpg?itok=2P8PYUJU",
+  },
+  {
+    id: 03,
+    name: "Pastel Tres Leches con Mousse",
+    price: 850,
+    realValue: 1200,
+    description:
+      "Esta es la torta o pastel tres leches mas rica que jamas probarás!! Si, lleva bastante trabajo por que son varias preparaciones por separado, pero valen la pena.",
+    stock: 2,
+    min: 1,
+    imagen:
+      "http://2.bp.blogspot.com/-YKfTLwn0QIE/WoZjR54aOYI/AAAAAAAABmQ/JPUES7CsJpM3LbyT_v-MWjzmOQKf5dHcgCK4BGAYYCw/s1600/tan%2Bdulce%2B3%2Bleches.jpg",
+  },
 ];
 
 module.exports = {
