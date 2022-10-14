a344b9693b7fbaea81e1f6d45dd06dca9daac7d4
Dedwison <dedwison@gmail.com>
2022-10-13 21:10:02 -0300

    json moularizado

---
+++ api/src/public/arrayManager.js
@@ -0,0 +1,21 @@
+let arrayManager = [
+  {
+    id: 01,
+    username: "pedro",
+    password: "abc123",
+  },
+  {
+    id: 02,
+    username: "juan",
+    password: "abc456",
+  },
+  {
+    id: 03,
+    username: "maria",
+    password: "abc789",
+  },
+];
+
+module.exports = {
+  arrayManager,
+};
