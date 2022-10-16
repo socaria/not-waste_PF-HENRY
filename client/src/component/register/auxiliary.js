6282c9547743111de48703d07348155636d145ec
Dedwison <dedwison@gmail.com>
2022-10-16 15:57:17 -0300

    cloudinary

--- client/src/component/register/auxiliary.js
+++ client/src/component/register/auxiliary.js
@@ -1,32 +1,50 @@
-export function validate (input) {
-    let error = {}
-    
-    if (!input.namesupplier) {
-        error.namesupplier = "Name is required";
-    }
-    else if (!/^[A-Za-z0-9\s]+$/g.test(input.namesupplier)) {
-        error.namesupplier = "Name is invalid. They only allow letters and numbers"
-    }
+export function validate(input) {
+  let error = {};
 
-    if (!input.phone) {
-        error.phone = 'Phone is required';
-    } else if (!/^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/g.test(input.phone)) {
-        error.phone = 'Enter a valid phone number'
-    }
+  if (!input.namesupplier) {
+    error.namesupplier = "Name is required";
+  } else if (!/^[A-Za-z0-9\s]+$/g.test(input.namesupplier)) {
+    error.namesupplier = "Name is invalid. They only allow letters and numbers";
+  }
 
-    if(input.category === '') {
-        error.category = 'Category is required'
-    }
+  if (!input.phone) {
+    error.phone = "Phone is required";
+  } else if (
+    !/^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/g.test(
+      input.phone
+    )
+  ) {
+    error.phone = "Enter a valid phone number";
+  }
 
-    if (!input.adress) {
-        error.adress = "Adress is required";
-    }
+  if (input.category === "") {
+    error.category = "Category is required";
+  }
 
-    if (!input.cuit) {
-        error.cuit = 'Cuit is required';
-    } else if (!/\b(20|23|24|27|30|33|34)(\D)?[0-9]{8}(\D)?[0-9]/g.test(input.cuit)) {
-        error.cuit = 'Enter a valid cuit'
-    }
+  if (!input.adress) {
+    error.adress = "Adress is required";
+  }
 
-    return error;
+  if (!input.cuit) {
+    error.cuit = "Cuit is required";
+  } else if (
+    !/\b(20|23|24|27|30|33|34)(\D)?[0-9]{8}(\D)?[0-9]/g.test(input.cuit)
+  ) {
+    error.cuit = "Enter a valid cuit";
+  }
+
+  return error;
 }
+
+export const widget_cloudinary = cloudinary.createUploadWidget(
+  {
+    cloudName: "ddb69vp6o",
+    uploadPreset: "not_waste",
+  },
+  (err, result) => {
+    if (!err && result && result.event === "success") {
+      console.log("Imagen subida con exito", result.info);
+      imagen.value = result.info.secure_url;
+    }
+  }
+);
