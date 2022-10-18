bbfdb7baf4e8f669474b2bfb3fc86713d233c37b
gabyakd2 <gabrielsal00@hotmail.com>
2022-10-18 20:04:17 -0300

    captura input formProduct y diets funcionando

--- client/src/component/FormProducts/index.jsx
+++ client/src/component/FormProducts/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { getDiet } from "../../redux/actions";
 
 function FormProduct() {
+  const dispatch = useDispatch()
   const [input, setInput] = useState({
     name: "",
     price: "",
@@ -13,24 +15,66 @@ function FormProduct() {
     description: "",
     stock: "",
   });
-  const diets = useSelector((state) => state.queryParams);
-  console.log(diets?.diets);
+  const diets = useSelector((state) => state.diet);
+  // console.log(diets)
+  console.log(input)
+
+  useEffect(() => {
+    dispatch(getDiet())
+  },[dispatch])
+
+  function handleInput(e){
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    })
+  }
 
   return (
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label></Form.Label>
-        <Form.Control type="text" placeholder="Nombre" />
+        <Form.Control 
+          name='name' 
+          value={input.name} 
+          type="text" 
+          placeholder="Nombre" 
+          onChange={(e) => handleInput(e)} />
         <Form.Label></Form.Label>
-        <Form.Control type="number" placeholder="Precio" />
+        <Form.Control 
+          name='price' 
+          value={input.price} 
+          type="number" 
+          placeholder="Precio" 
+          onChange={(e) => handleInput(e)} />
         <Form.Label></Form.Label>
-        <Form.Control type="number" placeholder="Valor Real" />
+        <Form.Control 
+          name='realValue' 
+          value={input.realValue} 
+          type="number" 
+          placeholder="Valor Real" 
+          onChange={(e) => handleInput(e)} />
         <Form.Label></Form.Label>
-        <Form.Control type="text" placeholder="Descripción" />
+        <Form.Control 
+          name='description' 
+          value={input.description} 
+          type="text" 
+          placeholder="Descripción" 
+          onChange={(e) => handleInput(e)} />
         <Form.Label></Form.Label>
-        <Form.Control type="number" placeholder="Stock" />
+        <Form.Control 
+          name='stock' 
+          value={input.stock} 
+          type="number" 
+          placeholder="Stock" 
+          onChange={(e) => handleInput(e)} />
         <Form.Label></Form.Label>
-        <Form.Control type="img" placeholder="Imagen del producto" />
+        <Form.Control 
+          name='image' 
+          value={input.image} 
+          type="file" 
+          placeholder="Imagen del producto" 
+          onChange={(e) => handleInput(e)} />
       </Form.Group>
 
       <Button type="submit">Enviar</Button>
