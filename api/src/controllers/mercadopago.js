a3593ff623ba41e51a15c59b5d22808d073db248
SerCM <serranocristian@hotmail.com.ar>
2022-10-19 19:56:54 -0300

    mp funcionando

--- api/src/controllers/mercadopago.js
+++ api/src/controllers/mercadopago.js
@@ -11,23 +11,23 @@ const post_create_preference = async (req, res) => {
       let preference = {
           items: [
               {
-                  title: req.body.title,
+                  title: "Not Waste",
                   unit_price: Number(req.body.price),
-                  quantity: Number(req.body.quantity)
+                  quantity: 1
               }
           ],
           back_urls: { //esto es donde redirecciona en cada caso
               "success": "http://localhost:3001/feedback",
               "failure": "http://localhost:3001/feedback",
               "pending": "http://localhost:3001/feedback"
-          },
+        },
           auto_return: "approved",
       };
-      console.log(preference)
       mercadopago.preferences.create(preference)
           .then(function (response) {
               res.json({
-                  id: response.body
+                  id: response.body.id,
+                  psgina_a_redireccionar: response.body.init_point
               });
           })
           .catch(function (error) {
@@ -36,12 +36,11 @@ const post_create_preference = async (req, res) => {
   };
   
   const get_feedback = async (req, res) => {  //mercadopago me redirecciona aca con los datos por query
-      // res.json({
-      // 	Payment: req.query.payment_id,
-      // 	Status: req.query.status,
-      // 	MerchantOrder: req.query.merchant_order_id
-      // });
-      res.json(req.query)
+      res.json({
+      	Status: req.query.status,
+        id: req.query.preference_id,
+        queryCompleto: req.query
+      });
   };
 
   module.exports = {
