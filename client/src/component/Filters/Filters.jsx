cb8c921c9a6fa66c05ceaf85b8b1f0b990a993d4
Sofia Caria <103151285+socaria@users.noreply.github.com>
2022-10-23 15:41:35 +0200

    filtros en offcanvas

--- client/src/component/Filters/Filters.jsx
+++ client/src/component/Filters/Filters.jsx
@@ -5,28 +5,50 @@ import Offcanvas from "react-bootstrap/Offcanvas";
 import { Button, Accordion, ListGroup, ListGroupItem } from "react-bootstrap";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useDispatch, useSelector } from "react-redux";
-import { postOrder, postPay, prodDetail } from "../../redux/actions";
+import { getSellers, postPay, prodDetail } from "../../redux/actions";
 
-function Filters(props) {
-  // const dispatch = useDispatch();
+function Filters() {
+  const dispatch = useDispatch();
   const [show, setShow] = useState(false);
-
-  // var { user } = useAuth0();
-
-  // let customers = useSelector((state) => state.customer);
+  let queryParams = useSelector(state => state.queryParams)
+  let cities = useSelector(state => state.cities)
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  // let cart = useSelector((state) => state.cart);
+  function handleCleanFilters(e) {
+    e.preventDefault();
+    window.location.reload();
+  }
 
-  // const productId = cart.productId;
+  function handleFilterCategory(category) {
+    dispatch(
+      getSellers({
+        ...queryParams,
+        category: category,
+      })
+    );
+  }
 
-  // const price = cart?.amount * cart?.price;
-  // const { isAuthenticated, loginWithRedirect } = useAuth0();
+  function handleFiltersPrice(price) {
+    dispatch(
+      getSellers({
+        ...queryParams,
+        price: price,
+      })
+    );
+  }
 
+  function handleFiltersCity(city) {
+    dispatch(
+      getSellers({
+        ...queryParams,
+        city: city,
+      })
+    );
+  }
 
-
+  let i = 0;
   return (
     <div>
       <div
@@ -37,7 +59,7 @@ function Filters(props) {
         onClick={handleShow}
       >
         <Button variant="light">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter-square" viewBox="0 0 16 16">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
             <path d="M6 11.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z" />
           </svg>
@@ -59,53 +81,48 @@ function Filters(props) {
           </Offcanvas.Header>
           <Offcanvas.Body >
             <div className="d-flex justify-content-center my-2">
-              <Button variant="secondary">Limpiar filtros</Button>
+              <Button onClick={(e) => handleCleanFilters(e)} variant="secondary">Limpiar filtros</Button>
             </div>
             <Accordion defaultActiveKey={['0']} alwaysOpen>
-              <Accordion.Item eventKey="0">
-                <Accordion.Header>Filtrar por ciudad</Accordion.Header>
-                <Accordion.Body>
-                  <ListGroup>
-                    <ListGroup.Item>
-                      Buenos Aires
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                      Córdoba
-                    </ListGroup.Item>
-                  </ListGroup>
+              <Accordion.Item eventKey='0'>
+                <Accordion.Header key='0'>Filtrar por precio</Accordion.Header>
+                <Accordion.Body className="d-flex row">
+                  <Button className="my-2" onClick={() => handleFiltersPrice('500')}>{'< $500'}</Button>
+                  <Button onClick={() => handleFiltersPrice('1000')}>{'< $1000'}</Button>
                 </Accordion.Body>
               </Accordion.Item>
-              <Accordion.Item eventKey="1">
-                <Accordion.Header>Filtrar por precios</Accordion.Header>
-                <Accordion.Body>
-                  <ListGroup>
-                    <ListGroup.Item>
-                      {" < $ 500"}
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                      {" < $ 1000"}
-                    </ListGroup.Item>
-                  </ListGroup>
+              <Accordion.Item eventKey='1'>
+                <Accordion.Header key='1'>Filtrar por categoría</Accordion.Header>
+                <Accordion.Body className="d-flex row">
+                  <Button onClick={() => handleFilterCategory('panaderia')}>Panadería</Button>
+                  <Button className="my-2" onClick={() => handleFilterCategory('supermercado')}>Supermercado</Button>
+                  <Button onClick={() => handleFilterCategory('restaurante')}>Restaurante</Button>
                 </Accordion.Body>
               </Accordion.Item>
-              <Accordion.Item eventKey="2">
-                <Accordion.Header>Filtrar por categoría</Accordion.Header>
-                <Accordion.Body>
-                  <ListGroup>
-                    <ListGroup.Item>
-                      Panadería
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                      Supermercado
-                    </ListGroup.Item>
-                  </ListGroup>
+              <Accordion.Item eventKey='2'>
+                <Accordion.Header key='2'>Filtrar por ciudad</Accordion.Header>
+                <Accordion.Body className="d-flex row">
+                  {cities?.map((city) => {
+                    console.log("🚀 ~ file: Filters.jsx ~ line 128 ~ {cities?.map ~ city", city)
+                    return (
+                      <Button
+                        onClick={() => handleFiltersCity(city.name)}
+                        key={city.id}
+                        className='cities-button'
+                      >
+                        {city.name}
+                      </Button>
+
+                    );
+                  })}
                 </Accordion.Body>
               </Accordion.Item>
+
             </Accordion>
           </Offcanvas.Body>
         </Offcanvas>
       </div>
-    </div>
+    </div >
   );
 }
 
--- client/src/component/Home/index.jsx
+++ client/src/component/Home/index.jsx
@@ -27,112 +27,48 @@ function Home() {
     dispatch(getDiet());
   }, [dispatch]);
 
-  function handleCleanFilters(e) {
-    e.preventDefault();
-    window.location.reload();
-  }
 
-  function handleFiltersCity(e) {
-    e.preventDefault();
-    dispatch(
-      getSellers({
-        ...queryParams,
-        city: e.target.value,
-      })
-    );
-  }
+  // function handleFiltersCity(e) {
+  //   e.preventDefault();
+  //   dispatch(
+  //     getSellers({
+  //       ...queryParams,
+  //       city: e.target.value,
+  //     })
+  //   );
+  // }
 
-  function handleFiltersPrice(e) {
-    e.preventDefault();
-    dispatch(
-      getSellers({
-        ...queryParams,
-        price: e.target.value,
-      })
-    );
-  }
+  // function handleFiltersPrice(e) {
+  //   e.preventDefault();
+  //   dispatch(
+  //     getSellers({
+  //       ...queryParams,
+  //       price: e.target.value,
+  //     })
+  //   );
+  // }
 
-  function handleFilterCategory(e) {
-    e.preventDefault();
-    dispatch(
-      getSellers({
-        ...queryParams,
-        category: e.target.value,
-      })
-    );
-  }
-  let currentCities = sellers.map((s) => s.cities.map((c) => c.name));
+  // function handleFilterCategory(e) {
+  //   e.preventDefault();
+  //   dispatch(
+  //     getSellers({
+  //       ...queryParams,
+  //       category: e.target.value,
+  //     })
+  //   );
+  // }
+  // let currentCities = sellers.map((s) => s.cities.map((c) => c.name));
   return (
     <div>
       <NavBar isSearchVisible />
       {errorMessage ? (
         <>
-          <button className="selects" onClick={(e) => handleCleanFilters(e)}>
-            Limpiar filtros
-          </button>
+          <Filters />
           <Message message={errorMessage} type="error" />
         </>
       ) : (
         <div className="container-fluid my-3">
-          <div>
-            <Filters />
-          </div>
-          <div className="contSelects">
-            {queryParams && (
-              <button
-                className="selects"
-                onClick={(e) => handleCleanFilters(e)}
-              >
-                Limpiar filtros
-              </button>
-            )}
-            <select
-              key={"filterBy_" + queryParams?.city}
-              value={queryParams?.city || "Ciudades"}
-              className="selects"
-              onChange={(e) => handleFiltersCity(e)}
-            >
-              <option>Ciudades</option>
-              {currentCities?.map((cities) => {
-                return (
-                  <option
-                    className="filter-options"
-                    value={cities}
-                    key={uuidv4()}
-                  >
-                    {cities}
-                  </option>
-                );
-              })}
-            </select>
-
-            <select onChange={(e) => handleFiltersPrice(e)} className="selects">
-              <option>Precios</option>
-              <option value="500">{`< $500`}</option>
-              <option value="1000">{`< $1000`}</option>
-            </select>
-
-            {/* <select onChange={handleFilterDiet} className="selects">
-                            <option>DIETA</option>
-                            {
-                                diet?.map(cities => {
-                                    return (
-                                        <option value={cities.name} key={cities.id}>{cities.name}</option>
-                                    )
-
-                                })
-                            }
-                        </select> */}
-            <select
-              onChange={(e) => handleFilterCategory(e)}
-              className="selects"
-            >
-              <option>Categoría</option>
-              <option value="panaderia">Panadería</option>
-              <option value="restaurante">Restaurante</option>
-              <option value="supermercado">Supermercado</option>
-            </select>
-          </div>
+          <Filters />
           {sellers?.map((seller) => {
             if (seller.products.find((p) => p.posts.length > 0))
               return <CarouselSeller key={seller.id} seller={seller} />;
